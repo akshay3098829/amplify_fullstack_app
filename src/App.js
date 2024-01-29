b42e53cf83789177bd47e5086009729c749d38d8
akshay3098829 <akshay.mohan@experionglobal.com>
2024-01-29 12:59:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "@aws-amplify/ui-react/styles.css"
+import {
+  Button,
+  Card,
+  Heading,
+  Image,
+  View,
+  withAuthenticator,
+} from "@aws-amplify/ui-react";
+function App({signOut}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <View>
+      <Card>
+        <Image src={logo} className='App-logo' alt ="logo"/>
+        <Heading level={1}>You have succesfully Login</Heading>
+      </Card>
+      <Button onClick={signOut}>Log Out</Button>
+    </View>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
